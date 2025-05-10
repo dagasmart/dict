68134c9b562b57473dbd41739a07f67233c0a569
raoxikui <raoxikui@qq.com>
2025-05-10 20:21:07 +0800

    初始化

--- src/Http/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use DagaSmart\Dict\Http\Controllers;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('/basic/dict', Controllers\BasicDictController::class);
-Route::get('/basic/dict/options', [Controllers\BasicDictController::class, 'dictOptions']);
-Route::get('/basic/dict/dict_type_options', [Controllers\BasicDictController::class, 'dictTypeOptions']);
