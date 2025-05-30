6065bb68a818f60570274e3e47c86401eeb491f8
raoxikui <raoxikui@qq.com>
2025-05-30 18:02:12 +0800

    优化

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,10 +1,25 @@
 <?php
 
 use DagaSmart\Dict\Http\Controllers;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+
+//需登录与鉴权
+//Route::group([
+//    'middleware' => config('admin.route.middleware'),
+//], function (Router $router) {
+//
+//    $router->get('/basic/dict/type_options', [Controllers\DictController::class, 'dictTypeOptions']);
+//    $router->get('/basic/dict/options', [Controllers\DictController::class, 'dictOptions']);
+//
+//    //resource必须放最后面
+//    $router->resource('/basic/dict', Controllers\DictController::class);
+//
+//});
 
 
 Route::get('/basic/dict/type_options', [Controllers\DictController::class, 'dictTypeOptions']);
 Route::get('/basic/dict/options', [Controllers\DictController::class, 'dictOptions']);
+
 //resource必须放最后面
 Route::resource('/basic/dict', Controllers\DictController::class);
