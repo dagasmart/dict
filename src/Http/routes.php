e347e8a6d31bc713e880676ddaf3bfb937b2e3a4
raoxikui <raoxikui@qq.com>
2026-07-09 17:29:48 +0800

    优化

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,17 +1,20 @@
 <?php
 
 use DagaSmart\Dict\Http\Controllers;
+use DagaSmart\Dict\Http\Middleware;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
-//需登录与鉴权
+// 需登录与鉴权
 Route::group([
     'prefix' => 'basic',
-    'middleware' => [],
+    'middleware' => [
+        Middleware\Middleware::class,
+    ],
 ], function (Router $router) {
     $router->get('dict/type_options', [Controllers\DictController::class, 'dictTypeOptions']);
     $router->get('dict/options', [Controllers\DictController::class, 'dictOptions']);
     $router->post('dict/save_order', [Controllers\DictController::class, 'saveOrder']);
-    //resource必须放最后面
+    // resource必须放最后面
     $router->resource('dict', Controllers\DictController::class);
 });
